9e535614d5679b3f857bc37f486cb418bf496342
wqndubuntu <wqsad>
2026-08-08 16:51:02 +0800

    checkpoint1 update1

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -6,31 +6,38 @@ using namespace std;
 void Reassembler::insert( uint64_t first_index, string data, bool is_last_substring )
 {
   // debug( "unimplemented insert({}, {}, {}) called", first_index, data, is_last_substring );
-  uint64_t max_index = next_idx_ + output_.writer().available_capacity();
-  for (uint64_t i = 0; i < (uint64_t)data.length(); i++) {
+  Writer& writer = output_.writer();
+
+  uint64_t max_index = next_idx_ + writer.available_capacity();
+  for (uint64_t i = 0; i < data.length(); i++) {
     uint64_t idx = first_index + i;
-    if (first_index + i < next_idx_ || first_index + i >= max_index || mp_.count(idx)) continue;
-    mp_[first_index + i] = data[i];
+    if (first_index + i < next_idx_ || first_index + i >= max_index) continue;
+    // mp_[first_index + i] = data[i];
+    mp_.try_emplace( idx, data[i] );
+
   }
   if (is_last_substring) {
     last_idx_ = first_index + data.length();
     has_last_ = 1;
   }
   string s;
-  while(mp_.count(next_idx_)) {
-    if ( output_.writer().available_capacity() == 0 ) {
-      break;
-    }
-    s += mp_[next_idx_];
-    mp_.erase(next_idx_);
+  while(1) {
+    auto it = mp_.find(next_idx_);
+    if (it == mp_.end()) break;
+    s += it->second;
+    mp_.erase(it);
     next_idx_++;
   }
-  output_.writer().push(s);
-  if (has_last_ && next_idx_ == last_idx_) output_.writer().close();
+  // if (!s.empty())
+    writer.push(s);
+  if (has_last_ && next_idx_ == last_idx_) writer.close();
 
  
 }
 
+
+
+// }
 // How many bytes are stored in the Reassembler itself?
 // This function is for testing only; don't add extra state to support it.
 uint64_t Reassembler::count_bytes_pending() const
